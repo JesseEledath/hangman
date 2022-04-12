bf1cba6d7c2ffea34ad99c56232f674a979a9995
Jesse Eledath <70730337+JesseEledath@users.noreply.github.com>
2022-04-12 13:39:19 -0400

    Merge pull request #1 from JesseEledath/api
    got api to print random word

--- src/main/java/com/lightwellinc/hangman/HangmanApplication.java
+++ src/main/java/com/lightwellinc/hangman/HangmanApplication.java
@@ -3,11 +3,21 @@ package com.lightwellinc.hangman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HangmanApplication {
+import java.io.IOException;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class HangmanApplication
+{
+
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(HangmanApplication.class, args);
+		game();
+	}
+
+	public static void game() throws IOException, InterruptedException {
+		GetApi api = new GetApi();
+		String word = api.GetApi();
+		System.out.println(word);
 	}
 
 }
